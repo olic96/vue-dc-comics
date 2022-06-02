ff064c8c0650b02469635ea795a95d2355c7ed28
olic96 <francoolich@gmail.com>
2022-06-02 12:30:09 +0200

    add img, structure Header, Main completed

---
+++ src/components/BaseMain.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+      <div class="container">
+            <p>--Content goes here-- </p>
+        </div>
+  </main>
+</template>
+
+<script>
+export default {
+    name: 'BaseMain',
+}
+</script>
+
+<style lang="scss" scoped>
+    main {
+        background-color: var(--secondary-color);
+        padding: 3.125rem;
+        color: #fff;
+        font-weight: 600;
+    }
+</style>
