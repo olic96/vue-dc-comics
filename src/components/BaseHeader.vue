eb6e6735ad0b06c9283c0534ce46af615bb8c568
olic96 <francoolich@gmail.com>
2022-06-02 15:56:44 +0200

    header

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,10 +1,10 @@
 <template>
  <div class="container">
     <header class="header">
-      <div class="logo">
+      <div class="header__logo">
           <img src="../assets/img/dc-logo.png">
       </div>
-      <nav class="header-nav">
+      <nav class="header__nav">
         <ul>
           <li><a href="#">link</a></li>
           <li><a href="#">link</a></li>
@@ -33,5 +33,27 @@ export default {
   .header{
     display: flex;
     justify-content: space-between;
+
+    &__nav {
+        ul {
+          display: flex;
+          list-style-type: none;
+          font-size: 1.25rem;
+
+          a {
+            display: inline-block;
+            text-decoration: none;
+            padding: 1.875rem 0.9375rem;
+            color: var(--secondary-color);
+            border-bottom: 0.3125rem solid transparent;;
+          }
+
+          :hover a {
+            color: var(--primary-color);
+            border-color: var(--primary-color);
+          }
+        }
+    }
+   
   }
 </style>
