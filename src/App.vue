12daa2c4de96bf7fa4a8053343d2315a7a20fcb1
olic96 <francoolich@gmail.com>
2022-06-02 12:31:30 +0200

    fix previous commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
-    
+    <BaseHeader/>
+    <BaseMain/>
   </div>
 </template>
 
 <script>
-
+import BaseHeader from './components/BaseHeader.vue';
+import BaseMain from './components/BaseMain.vue';
 
 export default {
   name: 'App',
   components: {
-   
+   BaseHeader,
+   BaseMain,
   }
 }
 </script>
 
 <style lang="scss">
-@import './assets/style/global.scss';
+  @import './assets/style/global.scss';
 </style>
---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,37 @@
+<template>
+ <div class="container">
+    <header class="header">
+      <div class="logo">
+          <img src="../assets/img/dc-logo.png">
+      </div>
+      <nav class="header-nav">
+        <ul>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+          <li><a href="#">link</a></li>
+        </ul>
+      </nav>
+  </header>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseHeader',
+
+}
+</script>
+
+<style lang="scss" scoped>
+  .header{
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
